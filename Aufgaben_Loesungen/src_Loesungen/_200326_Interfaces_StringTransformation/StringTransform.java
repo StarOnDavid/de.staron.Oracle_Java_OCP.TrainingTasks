a61d6f8411a931e2cdd5f5a1944c085ec18c3db5
David Staron <post.staron@gmail.com>
2020-03-26 15:43:24 +0100

    Lösung - Interfaces_StringTransformation update

--- Aufgaben_Loesungen/src_Loesungen/_200326_Interfaces_StringTransformation/StringTransform.java
+++ Aufgaben_Loesungen/src_Loesungen/_200326_Interfaces_StringTransformation/StringTransform.java
@@ -5,21 +5,7 @@ import java.util.Arrays;
 
 public class StringTransform {
 	
-	public static void main(String[] args) {
-		System.out.println("Loesung zu A1: \n");
-		String[] arr = {"mo", "di", "mi"};
-		String arrAsString = Arrays.toString(arr);
-		System.out.println("Array-Inhalt: "+ arrAsString);
-		ArrayList<String> list = transform(arr);
-		System.out.println("ArrayList-Inhalt: "+ list);
-		
-		System.out.println("\n\nLoesung zu A2: \n");
-		list = StringTransform.transform(arr, Transformation.CAPITALIZE);
-		System.out.println("ArrayList-Inhalt: "+ list);
-		
-	}
-	
-	protected static ArrayList<String> transform(String[] arr){
+	public static ArrayList<String> transform(String[] arr){
 		ArrayList<String> list = new ArrayList<String>();
 		for (String s : arr) {
 			list.add(s.toUpperCase());
@@ -27,7 +13,7 @@ public class StringTransform {
 		return list;
 	}
 	
-	protected static ArrayList<String> transform(String[] arr, Transformation t){
+	public static ArrayList<String> transform(String[] arr, Transformation t){
 		ArrayList<String> list = new ArrayList<String>();
 		switch (t) {
 		case UPPER:
