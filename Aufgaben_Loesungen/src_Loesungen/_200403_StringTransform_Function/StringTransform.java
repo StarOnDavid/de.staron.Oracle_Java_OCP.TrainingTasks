2f0d817cb2c5d00e588d78a73a37ededa889b88d
David Staron <post.staron@gmail.com>
2020-04-03 15:04:19 +0200

    Lösung - StringTransform_Function

---
+++ Aufgaben_Loesungen/src_Loesungen/_200403_StringTransform_Function/StringTransform.java
@@ -0,0 +1,24 @@
+package _200403_StringTransform_Function;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+
+public class StringTransform {
+	List<UnaryOperator<String>> functions = new ArrayList<UnaryOperator<String>>();
+	
+	public StringTransform addTransformation(UnaryOperator<String> stringFunction) {
+		functions.add(stringFunction);
+		return this;
+	}
+
+	public String process(String s) {
+		for(int i = 0; i < functions.size(); i++) {
+			s = functions.get(i).apply(s);
+		}
+		return s;
+	}
+}
---
+++ Aufgaben_Loesungen/src_Loesungen/_200403_StringTransform_Function/StringTransform_Function_run.java
@@ -0,0 +1,32 @@
+package _200403_StringTransform_Function;
+
+import java.util.function.UnaryOperator;
+
+public class StringTransform_Function_run {
+
+	public static void main(String[] args) {
+
+	    // Transformationen vordefinieren:
+	    StringTransform t1 = new StringTransform()
+	            .addTransformation( s -> s.toUpperCase() )
+	            .addTransformation( s -> s + "!" );
+	    
+	    // Transformationen durchführen:
+	    String s = t1.process("Hallo");
+	    System.out.println(s); // HALLO!
+	    
+	    // Transformationen mit einem anderen String durchführen:
+	    s = t1.process("Java ist toll");
+	    System.out.println(s); // JAVA IST TOLL!
+	    
+	    
+	    StringTransform t2 = new StringTransform()
+	            .addTransformation( x -> x.substring(7) )
+	            .addTransformation( x -> x.concat(" ist") )
+	            .addTransformation( x -> x.concat(" mein Name.") );
+	    
+	    // Transformationen durchführen:
+	    String s2 = t2.process("Ulrich Hase");
+	    System.out.println(s2); // Hase ist mein Name.
+	}
+}
