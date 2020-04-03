10d1604c540dd3d69ace515b0d1fc2b0d364d57d
David Staron <post.staron@gmail.com>
2020-04-03 15:14:58 +0200

    Lösung - PutIntegers_Consumer

---
+++ Aufgaben_Loesungen/src_Loesungen/_200403_PutIntegers_Consumer/PutIntegers_Consumer_run.java
@@ -0,0 +1,29 @@
+package _200403_PutIntegers_Consumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class PutIntegers_Consumer_run {
+	
+	public static void main(String[] args) {
+
+		StringBuilder sb = new StringBuilder();
+		putIntegers( i -> sb.append(i).append(" ") );
+		System.out.println(sb); 	// Zeile A
+
+		
+		List<Integer> list = new ArrayList<>();
+		putIntegers( i -> list.add(i) );
+		System.out.println(list); // Zeile B
+
+		
+		putIntegers( System.out::print ); 	// Zeile C, optionale Aufgabe, 
+													// die Zeile C kann entfernt werden
+	}
+	
+	public static void putIntegers(Consumer<Integer> con) {
+		for(int i = 1; i < 5; i++)
+			con.accept(i);
+	}
+}
