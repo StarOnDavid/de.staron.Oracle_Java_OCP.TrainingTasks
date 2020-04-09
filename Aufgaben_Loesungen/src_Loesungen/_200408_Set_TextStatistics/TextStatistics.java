4136b073b0afd3a2d1a40da0db284d5a7fea5a83
David Staron <post.staron@gmail.com>
2020-04-09 10:53:55 +0200

    Set - TextStatistics update

--- Aufgaben_Loesungen/src_Loesungen/_200408_Set_TextStatistics/TextStatistics.java
+++ Aufgaben_Loesungen/src_Loesungen/_200408_Set_TextStatistics/TextStatistics.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class TextStatistics {
-	String text;
+	private String text;
 
 	public static TextStatistics of(String text) {
 		TextStatistics obj = new TextStatistics();
