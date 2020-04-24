4ea8aa3049d5df4183cc42e1da629303e233a43b
David Staron <post.staron@gmail.com>
2020-04-24 13:40:50 +0200

    Lösung - MiniTask - 20.04.2020

---
+++ Aufgaben_Loesungen/src_Loesungen/_200424_MiniTask_Stream/Stream_anyAllMatch.java
@@ -0,0 +1,38 @@
+package _200424_MiniTask_Stream;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Stream_anyAllMatch {
+
+	public static void main(String[] args) {
+		String[] woche = {
+				"Montag",
+				"Dienstag",
+				"Mittwoch",
+				"Donnerstag",
+				"Freitag",
+				"Samstag",
+				"Sonntag"
+		};
+		
+		Stream<String> laengerA = Stream.of(woche)
+				.filter(x -> x.length() > 8);
+		
+		long anz = laengerA.count();
+		System.out.println("In Woche gibt es " + anz + " String(s) der/die mehr als 8 Zeichen haben.");
+		
+		boolean gibtMitMehrAlsAcht = Stream.of(woche)
+				.anyMatch(s -> s.length() > 8);
+		System.out.println("In Woche gibt es Strings die mehr als 8 Zeichen haben: "+ gibtMitMehrAlsAcht);
+		
+		boolean mit_t = Stream.of(woche).allMatch(s -> s.contains("t"));
+		System.out.println("In Woche enthalten alle Strings ein 't': "+ mit_t);
+		
+		boolean startNotWithA = Stream.of(woche).allMatch(s -> s.charAt(0) != 'A');
+		System.out.println("In Woche beginnt kein String mit 'A': "+ startNotWithA);
+
+		
+		
+	}
+}
